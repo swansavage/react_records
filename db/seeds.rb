bac8965bca846dbf4f33d28cfb5b3fc04c31a20b
Shaun Savage <swansavage@gmail.com>
2018-04-30 22:38:22 -0400

    testing db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,7 @@
 
 
 
-if(ENV['DATABASE_URL'])
-    uri = URI.parse(ENV['DATABASE_URL'])
-    DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
-else
-    DB = PG.connect(host: "localhost", port: 5432, dbname: 'record_store')
-end
+DB = PG.connect(host: "localhost", port: 5432, dbname: 'record_store')
 
 
 DB.exec(
